4120e502621358232fc1c4827b87b8db90dbbf08
Nobuo Kato <katonobu4649@gmail.com>
2023-10-20 00:06:00 +0900

    Fix runtime error.

--- notebooks/calc_sat_view.py
+++ notebooks/calc_sat_view.py
@@ -1,8 +1,13 @@
 import skyfield.api
 from skyfield.api import wgs84
 from datetime import datetime, timezone, timedelta
+from skyfield.api import Loader
 
-def calc_sat_view(dt=datetime.now(), tz=timezone(timedelta(hours=9)), view_from_lat = 35.400334, view_from_lon = 139.543152):
+def calc_sat_view(dt=None, tz=timezone(timedelta(hours=9)), view_from_lat = 35.400334, view_from_lon = 139.543152, force_reload = False):
+    if dt is None:
+        dt = datetime.now(tz)
+    input_params = {'dt':dt, 'tz':tz, 'view_from':{'lat':view_from_lat,'lon':view_from_lon}}
+
     # 表示する最低仰角
     min_disp_alt_deg = 5
 
@@ -18,54 +23,86 @@ def calc_sat_view(dt=datetime.now(), tz=timezone(timedelta(hours=9)), view_from_
         'NAVSTAR',
         'QZS',
     ]
+    # 衛星軌道情報download URL
+    url = "https://celestrak.org/NORAD/elements/gnss.txt"
 
-    sats = skyfield.api.load.tle_file("https://celestrak.org/NORAD/elements/gnss.txt", reload=True)
-    rounded_datetime = datetime.fromtimestamp(((dt.timestamp() + interval_sec) // interval_sec) * interval_sec, tz=tz)
+    internal_params = {
+        'min_disp_alt_deg':min_disp_alt_deg,
+        'interval_sec':interval_sec,
+        'past_generate_count':past_generate_count,
+        'future_generate_count':future_generate_count,
+        'calc_sats':calc_sats,
+        'url':url
+    }
 
-    ts = skyfield.api.load.timescale()
-    tss = ts.from_datetimes(
-        [rounded_datetime - timedelta(minutes=5 * i) for i in range(-past_generate_count,future_generate_count+1)]
-    )
-    current_ts = ts.from_datetime(rounded_datetime)
-    view_from = wgs84.latlon(view_from_lat,view_from_lon) 
     calc_results = []
-    for sat in sats:
-        sat_name = ''
-        for sat_key in calc_sats:
-            if sat.name.startswith(sat_key):
-                sat_name = sat.name
-                break
-        if 0 < len(sat_name):
-            sat_calc_result = {'name':sat_name, 'pos':[], 'cpos':None}
-            alts = []
-            azs = []
-            difference = sat - view_from
-            prev_appended = False
-            for target_ts in tss:
+    rounded_datetime = datetime.fromtimestamp(((dt.timestamp() + interval_sec) // interval_sec) * interval_sec, tz=tz)
+    intermediate_params = {
+        'rounded_datetime':rounded_datetime
+    }
+
+    load = Loader('.', verbose=False)
+    try:
+        sats = load.tle_file(url, reload=force_reload)
+
+        ts = skyfield.api.load.timescale()
+        tss = ts.from_datetimes(
+            [rounded_datetime - timedelta(minutes=5 * i) for i in range(-past_generate_count,future_generate_count+1)]
+        )
+        current_ts = ts.from_datetime(rounded_datetime)
+        view_from = wgs84.latlon(view_from_lat,view_from_lon) 
+        for sat in sats:
+            sat_name = ''
+            for sat_key in calc_sats:
+                if sat.name.startswith(sat_key):
+                    sat_name = sat.name
+                    break
+            if 0 < len(sat_name):
+                sat_calc_result = {'name':sat_name, 'pos':[], 'cpos':None}
+                alts = []
+                azs = []
+                difference = sat - view_from
+                prev_appended = False
+                for target_ts in tss:
+                    topocentric = difference.at(target_ts)
+                    alt, az, _ = topocentric.altaz()
+                    if min_disp_alt_deg < alt.degrees:
+                        alts.append(alt.degrees)
+                        azs.append(az.degrees)
+                        prev_appended = True
+                    else:
+                        if prev_appended and 0 < len(alts) and 0 < len(azs):
+                            prev_appended = False
+                            sat_calc_result['pos'].append({'alts':alts.copy(), 'azs':azs.copy()})
+                            alts = []
+                            azs = []
+                if 0 < len(alts) and 0 < len(azs):
+                    sat_calc_result['pos'].append({'alts':alts.copy(), 'azs':azs.copy()})
+                    alts = []
+                    azs = []
+
+                target_ts = current_ts
                 topocentric = difference.at(target_ts)
                 alt, az, _ = topocentric.altaz()
                 if min_disp_alt_deg < alt.degrees:
-                    alts.append(alt.degrees)
-                    azs.append(az.degrees)
-                    prev_appended = True
-                else:
-                    if prev_appended and 0 < len(alts) and 0 < len(azs):
-                        prev_appended = False
-                        sat_calc_result['pos'].append({'alts':alts.copy(), 'azs':azs.copy()})
-                        alts = []
-                        azs = []
-            if 0 < len(alts) and 0 < len(azs):
-                sat_calc_result['pos'].append({'alts':alts.copy(), 'azs':azs.copy()})
-                alts = []
-                azs = []
+                    sat_calc_result['cpos'] = {'az':az.degrees, 'alt':alt.degrees}
+                    calc_results.append(sat_calc_result.copy())
+                elif 0 < len(sat_calc_result['pos']):
+                    calc_results.append(sat_calc_result.copy())
+    except Exception as e:
+        print(e)
 
-            target_ts = current_ts
-            topocentric = difference.at(target_ts)
-            alt, az, _ = topocentric.altaz()
-            if min_disp_alt_deg < alt.degrees:
-                sat_calc_result['cpos'] = {'az':az.degrees, 'alt':alt.degrees}
-                calc_results.append(sat_calc_result.copy())
-            elif 0 < len(sat_calc_result['pos']):
-                calc_results.append(sat_calc_result.copy())
+    return {'calc_results':calc_results, 'input_params':input_params, 'internal_params':internal_params, 'intermediate_params':intermediate_params}
 
-    return calc_results
+
+if __name__ == "__main__":
+    import json
+    def json_serial(obj):
+        if isinstance(obj, (datetime)):
+            return obj.isoformat()
+        if isinstance(obj, (timezone)):
+            return obj.tzname(None)
+        raise TypeError ("Type %s not serializable" % type(obj))
+
+    calc_results = calc_sat_view()
+    print(json.dumps(calc_results, default=json_serial, indent=2))
--- notebooks/draw_sat_view.py
+++ notebooks/draw_sat_view.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 import math
 
-def draw_sat_view(calc_results, dt, view_from_lat = 35.400334, view_from_lon = 139.543152, calc_sats=['NAVSTAR','QZS']):
+def draw_sat_view(calc_results):
+    dt = calc_results['intermediate_params']['rounded_datetime']
+    view_from_lat = calc_results['input_params']['view_from']['lat']
+    view_from_lon = calc_results['input_params']['view_from']['lon']
+    calc_sats = calc_results['internal_params']['calc_sats']
+
     marker_tables = [",","o","v","^","<",">",]
     shape_table = { name:marker_tables[i] for i, name in enumerate(calc_sats)}
 
@@ -13,12 +18,12 @@ def draw_sat_view(calc_results, dt, view_from_lat = 35.400334, view_from_lon = 1
 
     rtick_deg = [90,60,45,30,0]
     ax.set_rticks([math.cos(deg/180 * math.pi) for deg in rtick_deg],[str(deg) for deg in rtick_deg])
-    plt.title("Satellite view from [{},{}] at {}".format(view_from_lat, view_from_lon, dt.strftime('%Y/%m/%d %H:%M:%S')))
+    plt.title(f"Satellite view from [{view_from_lat},{view_from_lon}] at {dt}")
 
     cmap = plt.get_cmap("tab10")
     cidx = 0
 
-    for result in calc_results:
+    for result in calc_results['calc_results']:
         shape = ''
         for sat_key in shape_table.keys():
             if result['name'].startswith(sat_key):
@@ -36,4 +41,12 @@ def draw_sat_view(calc_results, dt, view_from_lat = 35.400334, view_from_lon = 1
             cidx += 1
             cidx %= 10
 
-    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1),fontsize="xx-small")
+
+if __name__ == "__main__":
+  import json
+  import os
+  json_file_path = os.path.join(os.path.dirname(__file__), 'sample_result_view.json')
+  with open(json_file_path, 'r') as f:
+    draw_sat_view(json.load(f))
+    plt.show()
